d23f9cd1f0056e1b6a21afce9f9c6241674ab4d5
Mickey <mikolajkarla@gmail.com>
2024-02-21 19:56:53 +0100

    ended questions. Only remains to end scorepage and score script

--- 3a_quiz-master/public/score.js
+++ 3a_quiz-master/public/score.js
@@ -1,26 +1,37 @@
 var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    var yValues = [55, 49, 44, 24, 15];
-    var barColors = [
-      "#b91d47",
-      "#00aba9",
-      "#2b5797",
-      "#e8c3b9",
-      "#1e7145"
-    ];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
-    new Chart("myChart", {
-      type: "pie",
-      data: {
-        labels: xValues,
-        datasets: [{
-          backgroundColor: barColors,
-          data: yValues
-        }]
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
       },
-      options: {
-        title: {
-          display: true,
-          text: "Wyniki quizu"
-        }
-      }
-    });
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Wyniki quizu",
+    },
+  },
+});
+
+let answers = JSON.parse(localStorage.getItem("answers"));
+console.log(answers);
+
+let users = [];
+//do poprawy wynik i sumowanie dla kazdego uzytkownika, a pozniej dodanie do diagramu i wyswietlenie go!
+answers.forEach((element) => {
+  if (element.username in users) {
+    users.push(element.username);
+    console.log("dodalem: ", element.username);
+  } else {
+    console.log("juz jest!  ", users, element.username);
+  }
+});
+console.log(users);
--- 3a_quiz-master/public/script.js
+++ 3a_quiz-master/public/script.js
@@ -1,109 +1,125 @@
-progressBar = document.querySelector(".w3-grey");
-timeValue = 0;
-timer = 0
-changeQuestion = 0
+let progressBar = document.querySelector(".w3-grey");
+let timeValue = 0;
+let timer = 0;
+let changeQuestion = 0;
+let questionIndex = 0
 
-let UID = window.localStorage.getItem("UID")
+let UID = window.localStorage.getItem("UID");
 if (!UID) {
-    UID = Math.floor(Math.random() * 100000000000) + `_` + (new Date).getTime();
-    window.localStorage.setItem("UID", UID);
+  UID = Math.floor(Math.random() * 100000000000) + `_` + new Date().getTime();
+  window.localStorage.setItem("UID", UID);
 }
-console.log(UID)
+// console.log(UID);
 
-let connect = io('ws://localhost:3000')
-connect.on('connect', () => {
-    console.log("Połączono", connect.id)
-})
-let username = ""
+let connect = io("ws://localhost:3000");
+connect.on("connect", () => {
+  console.log("Połączono", connect.id);
+});
+let username = "";
 
 // pytanie o imie usera
-connect.on('username', () => {
-    if (!username) {
-        username = prompt("Podaj swoje imie")
-    }
-    connect.emit('username', username, UID);
-})
+connect.on("username", () => {
+  if (!username) {
+    username = prompt("Podaj swoje imie");
+  }
+  connect.emit("username", username, UID);
+});
 
-
-
-
-let pytania = []
-const $ = name => document.querySelector(name)
+let pytania = [];
+const $ = (name) => document.querySelector(name);
 
 const czyt_pyt = () => {
-    fetch("pytania.json")
-        .then(e => e.json())
-        .then(j => {
-            pytania = []
-            pytania.push(...j)
-            losuj_pyt()
-        })
-}
+  fetch("pytania.json")
+    .then((e) => e.json())
+    .then((j) => {
+      pytania = [];
+      pytania.push(...j);
+      losuj_pyt();
+    });
+};
+let answer = "";
+let pytanie = {};
 
 const losuj_pyt = () => {
-  let pytanie = pytania.filter(e => !e.wylosowane)[0]
+  pytanie = pytania.filter((e) => !e.wylosowane)[0];
   if (!pytanie) {
-      czyt_pyt()
+    czyt_pyt();
   }
-  pytanie.wylosowane = true
-  $('.pytanie').innerHTML = pytanie.pytanie
+  pytanie.wylosowane = true;
+  $(".pytanie").innerHTML = pytanie.pytanie;
   for (let i = 0; i < pytanie.odp.length; i++) {
-      $(`.p${i}`).innerHTML = pytanie.odp[i]
+    $(`.p${i}`).innerHTML = pytanie.odp[i];
+    $(`.p${i}`).addEventListener("click", () => {
+      answer = i;
+    //   console.log(answer);
+    });
   }
-}
+};
 
-czyt_pyt()
+czyt_pyt();
 
-let currentQuestionIndex = 0
-let questions = []
-
-const findElement = (selector) => document.querySelector(selector)
-
-
+let questions = [];
 
 const saveAnswer = (questionIndex, isCorrect) => {
-    let answers = JSON.parse(localStorage.getItem('answers')) || []
+  let answers = JSON.parse(localStorage.getItem("answers")) || [];
 
-    const savedUsername = JSON.parse(localStorage.getItem("username"))
+  const savedUsername = JSON.parse(localStorage.getItem("username"));
 
-    const socketUsername = username
+  const socketUsername = username;
 
-    const finalUsername = savedUsername || socketUsername || "Unknown"
+  const finalUsername = savedUsername || socketUsername || "Unknown";
 
-    answers.push({ username: finalUsername, index: questionIndex, correct: isCorrect })
+  answers.push({
+    username: finalUsername,
+    index: questionIndex,
+    correct: isCorrect,
+  });
 
-    localStorage.setItem('answers', JSON.stringify(answers))
-}
+  localStorage.setItem("answers", JSON.stringify(answers));
+};
 
 const nextQuestion = () => {
-    currentQuestionIndex++
-    console.log(currentQuestionIndex,pytania.length);
-    if (currentQuestionIndex < pytania.length) {
-        timeValue=0
-        czyt_pyt()
-    } else {
-        window.location.href = "/ScorePage.html"
-    }
-}
+//   console.log(pytanie);
+
+  if (answer == pytanie.poprawna) {
+    saveAnswer(questionIndex, 1);
+    // console.log("masz punkt");
+  } else {
+    saveAnswer(questionIndex, 0);
+    // console.log("nie masz");
+  }
+  answer =  null;
+//   console.log(questionIndex, pytania.length);
+  if (questionIndex < pytania.length) {
+    timeValue = 0;
+    questionIndex++;
+    losuj_pyt();
+  } else {
+    window.location.href = "/ScorePage.html";
+  }
+};
+
 
 const startTimer = () => {
-    clearInterval(timer);
-    clearInterval(changeQuestion);
-    
-    timer = setInterval(countdown, 1000);
-    changeQuestion = setInterval(nextQuestion, 11000);
-}
-const countdown = () => {
-  let percentage = (timeValue)+'%'
-  progressBar.style.width=percentage
-  timeValue += 10
+  clearInterval(timer);
+  clearInterval(changeQuestion);
 
-}
-startTimer()
+  timer = setInterval(countdown, 1000);
+  changeQuestion = setInterval(nextQuestion, 10000);
+};
+
+
+const countdown = () => {
+  let percentage = timeValue + "%";
+  progressBar.style.width = percentage;
+  timeValue += 10;
+};
+
+startTimer();
 
 
 
 window.onload = () => {
-    connect.emit('getUsername')
-    connect.emit('loadQuestions')
-}
+  connect.emit("getUsername");
+  connect.emit("loadQuestions");
+};
